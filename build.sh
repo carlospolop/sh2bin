16b1ba0e896b2422dc45875c47e830c8c53529f0
Carlos Polop <carlospolop@gmail.com>
2021-12-29 17:20:17 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,11 +15,11 @@ base64 -w0 "$1" > script.sh.b64
 goarchList="386 amd64 arm arm64" #ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc s390 s390x sparc sparc64"
 goos="linux darwin"
 
-for os in goos; do
+for os in $goos; do
     for arch in $goarchList; do
         echo "Building for $arch"
         env GOOS=$os GOARCH=$arch go build -o builds/sh2bin_$os_$arch
-        chmod +x builds/sh2bin_$arch
+        chmod +x builds/sh2bin_$os_$arch
     done
 done
 
